e6ea44601dbd30d4463442b02e0a24e3f84b77bf
MichaelJCompton <michael@dgraph.io>
2020-08-07 16:37:46 +1000

    Add single user Todo app.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,152 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { useQuery, useMutation, gql } from "@apollo/client"
+import { Todos } from "react-todomvc"
+
+import "react-todomvc/dist/todomvc.css"
+
+const GET_TODOS = gql`
+  query {
+    queryTodo {
+      id
+      value
+      completed
+    }
+  }
+`
+
+const ADD_TODO = gql`
+  mutation addTodo($todo: AddTodoInput!) {
+    addTodo(input: [$todo]) {
+      todo {
+        id
+        value
+        completed
+      }
+    }
+  }
+`
+
+const UPDATE_TODO = gql`
+  mutation updateTodo($id: ID!, $todo: TodoPatch!) {
+    updateTodo(input: { filter: { id: [$id] }, set: $todo }) {
+      todo {
+        id
+        value
+        completed
+      }
+    }
+  }
+`
+
+const DELETE_TODO = gql`
+  mutation deleteTodo($id: ID!) {
+    deleteTodo(filter: { id: [$id] }) {
+      todo {
+        id
+      }
+    }
+  }
+`
+
+const CLEAR_COMPLETED_TODOS = gql`
+  mutation updateTodo {
+    deleteTodo(filter: { completed: true }) {
+      todo {
+        id
+      }
+    }
+  }
+`
 
 function App() {
+  const [add] = useMutation(ADD_TODO)
+  const [del] = useMutation(DELETE_TODO)
+  const [upd] = useMutation(UPDATE_TODO)
+  const [clear] = useMutation(CLEAR_COMPLETED_TODOS)
+
+  const { loading, error, data } = useQuery(GET_TODOS)
+  if (loading) return <p>Loading</p>
+  if (error) {
+    return <p>`Error: ${error.message}`</p>
+  }
+
+  const addNewTodo = (value) =>
+    add({
+      variables: { todo: { value: value, completed: false } },
+      update(cache, { data }) {
+        const existing = cache.readQuery({ query: GET_TODOS })
+        cache.writeQuery({
+          query: GET_TODOS,
+          data: {
+            queryTodo: [
+              ...(existing ? existing.queryTodo : []),
+              ...data.addTodo.todo,
+            ],
+          },
+        })
+      },
+    })
+
+  const updateTodo = (modifiedTodo) =>
+    upd({
+      variables: {
+        id: modifiedTodo.id,
+        todo: {
+          value: modifiedTodo.value,
+          completed: modifiedTodo.completed,
+        },
+      },
+
+      update(cache, { data }) {
+        const existing = cache.readQuery({ query: GET_TODOS })
+        const modifiedTodos = existing.queryTodo.map((t) => {
+          if (t.id === modifiedTodo.id) {
+            return modifiedTodo
+          } else {
+            return t
+          }
+        })
+        cache.writeQuery({
+          query: GET_TODOS,
+          data: { queryTodo: modifiedTodos },
+        })
+      },
+    })
+
+  const deleteTodo = (id) =>
+    del({
+      variables: { id },
+      update(cache, { data }) {
+        const existingTodos = cache.readQuery({ query: GET_TODOS })
+        const newTodos = existingTodos.queryTodo.filter((t) => t.id !== id)
+        cache.writeQuery({
+          query: GET_TODOS,
+          data: { queryTodo: newTodos },
+        })
+      },
+    })
+
+  const clearCompletedTodos = () =>
+    clear({
+      refetchQueries: [
+        {
+          query: GET_TODOS,
+        },
+      ],
+    })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Todos
+        todos={data.queryTodo}
+        addNewTodo={addNewTodo}
+        updateTodo={updateTodo}
+        deleteTodo={deleteTodo}
+        clearCompletedTodos={clearCompletedTodos}
+        todosTitle="GraphQL Todos"
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client"
+
+const createApolloClient = () => {
+  const httpLink = createHttpLink({
+    uri: "https://urbane-powder-1224.us-west-2.aws.cloud.dgraph.io/graphql",
+  })
+
+  return new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
+  })
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  <ApolloProvider client={createApolloClient()}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
+  document.getElementById("root")
+)
